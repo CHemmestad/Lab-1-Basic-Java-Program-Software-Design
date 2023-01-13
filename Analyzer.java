a993d4a5a9be01064a86b3f067594be9daad9e16
Caleb <73370112+CHemmestad@users.noreply.github.com>
2023-01-13 12:56:27 -0600

    Update Analyzer.java

--- Analyzer.java
+++ Analyzer.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
 
 public class Analyzer {
+    //Precondition : Dont know which element in the array is the largest
+    //Postcondition : Searches array to find largest element and prints result
     public static void Max(ArrayList<Float> list) {
-        int size = list.size();
+        int size = list.size(); //Finds the size of the array for the average calculations
 
         float max = '\0';
 
         for (int x = 0 ; x < size ; x++) {
             float num = list.get(x);
             if ( max == '\0' || num > max ) {
-                max = num;
+                max = num; //Saves the largest element as it goes through the array
             }
         }
 
-        System.out.println(max);
+        System.out.println(max); //Prints the results
     }
+    //Precondition : Dont know which element in the array is the smallest
+    //Postcondition : Searches array to find the smallest element and prints result
     public static void Min(ArrayList<Float> list) {
         int size = list.size();
 
@@ -23,12 +27,14 @@ public class Analyzer {
         for (int x = 0 ; x < size ; x++) {
             float num = list.get(x);
             if ( min == '\0' || num < min ) {
-                min = num;
+                min = num; //Saves the smallest element as it goes through the array
             }
         }
 
-        System.out.println(min);
+        System.out.println(min); //Prints the results
     }
+    //Precondition : Need to calculate the average from all the elements in the array
+    //Postcondition : Adds all the elements of the array together, devides by size of array, then prints results
     public static void Avg(ArrayList<Float> list) {
         int size = list.size();
 
@@ -40,11 +46,11 @@ public class Analyzer {
                 avg = num;
             }
             else {
-                avg += num;
+                avg += num; //Adds the elements of the array as it goes through array
             }
         }
 
-        avg = avg/size;
-        System.out.println(avg);
+        avg = avg/size; //Divides the total by the size of the array for the avg
+        System.out.println(avg); //Prints the results
     }
 }
