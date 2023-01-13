a11fff9ff9859354e2c05ae18ac53b0b0068cfa2
Caleb <73370112+CHemmestad@users.noreply.github.com>
2023-01-13 12:57:30 -0600

    Update AnalyzerTester.java

--- AnalyzerTester.java
+++ AnalyzerTester.java
@@ -1,7 +1,7 @@
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.IOException; //Needed for case of failure
+import java.nio.file.Paths; //Needed for locating and opening the file
+import java.util.Scanner; //Needed for inout and outputs from and to the file
+import java.util.ArrayList; //Needed for making the array
 
 public class AnalyzerTester {
     public static void main (String[] args) throws IOException {
@@ -13,13 +13,15 @@ public class AnalyzerTester {
 
         ArrayList<Float> List = ReadFile(file);
         
-        Analyzer.Max(List);
-        Analyzer.Min(List);
-        Analyzer.Avg(List);
+        Analyzer.Max(List); //Finds and prints the max in the array
+        Analyzer.Min(List); //Finds and prints the min in the array
+        Analyzer.Avg(List); //Adds all elements together and devides by size then prints avg
 
-        List.clear();
+        List.clear(); //Deletes the array when finished
     }
 
+    //Precondition : Dont have a way of reading the file and making the array
+    //Postcondition : Reads the file and creates array
     public static ArrayList<Float> ReadFile(String fileName) throws IOException {
         Scanner inFile = new Scanner(Paths.get(fileName),"UTF-8") ;
         
@@ -28,7 +30,7 @@ public class AnalyzerTester {
         do {
             float x = inFile.nextInt();
             List.add(x);
-        } while (inFile.hasNext()) ;
+        } while (inFile.hasNext()) ; //Checks to see if we reached the end of the file
 
         inFile.close();
 
